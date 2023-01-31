5a5333151bd81866e58191a4570d0efa4bb3c291
zhendrix3019 <114441063+zhendrix3019@users.noreply.github.com>
2023-01-30 20:31:12 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+            // display a literal value
+            Console.WriteLine("What is your name?");
+            // input a value and assign it to a string variable
+            string? name = Console.ReadLine();
+            // display the string variable
+            Console.WriteLine("Hello, " + name);
+            Console.WriteLine("Hello, {0}", name);
+        
